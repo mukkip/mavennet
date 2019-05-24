89a641e11ead91439c673cf602f5114a588c73f5
muksh.developer@gmail.com <muksh.developer@gmail.com>
2019-05-24 09:27:25 -0700

    Updates

---
+++ precache-manifest.293674bcd23c3e74ab0f4903fdedcf47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77d423b3e190d609752565d712ab04c",
+    "url": "/mavennet/index.html"
+  },
+  {
+    "revision": "a7fc1cf44225bf16f172",
+    "url": "/mavennet/static/css/main.1bbc9291.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/mavennet/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "a7fc1cf44225bf16f172",
+    "url": "/mavennet/static/js/main.35a073b4.chunk.js"
+  },
+  {
+    "revision": "14fdc79b9900d2249224",
+    "url": "/mavennet/static/js/runtime~main.c10fbfea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mavennet/static/media/logo.5d5d9eef.svg"
+  }
+]);
